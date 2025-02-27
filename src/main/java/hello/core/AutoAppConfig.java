7794bbeddc0a4f09e995e2c36b1ad9b1036384f8
Yunho Ju <ju020620@naver,com>
2025-02-27 15:27:48 +0900

    forth commit

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,37 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//      basePackages = "hello.core.member",
+//      탐색할 패키지의 시작 위치를 지정
+//      basePackageClasses = AutoAppConfig.class,
+//      지정한 클래스의 패키지를 탐색 시작 위치로 지정
+/*      ** default **  만약 지정하지 않으면
+        @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
+        => 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두도록 하자!
+ */
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // 보통 설정 정보를 컴포넌트 스캔 대상에서 제외하지는 않지만,
+        // 기존 예제 코드를 유지하기 위해서 이 방법을 선택.
+)
+
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    /*
+    이 경우 수동 빈 등록이 우선권을 가진다.
+    => 수동 빈이 자동 빈을 오버라이딩 해버림.
+     */
+}
+
+
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 //  MemberServiceImpl은 MemberRepository(추상화) 에도 의존하고, MemoryMemberRepository(구체화)에도 의존
 //  => DIP 위반!
@@ -10,6 +14,14 @@ public class MemberServiceImpl implements MemberService{
     오로지 MemberRepository(인터페이스)에 대한 것만 있음, 추상화에만 의존
     => DIP를 잘 지킴.
  */
+    @Autowired
+    /*
+    ac.getBean(MemberRepository.class)
+       ComponentScan을 쓰면 Autowired를 쓰게 됨
+       Why? 내 Bean이 자동으로 등록은 되는데 의존 관계를 설정할 수 있는 방법이 없음
+       수동으로 등록할 수 있는 장소가 없기 때문...
+       => @Autowired는 의존관계를 자동으로 주입해준다.
+     */
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
